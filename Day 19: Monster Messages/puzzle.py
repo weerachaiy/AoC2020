5c2f6e472fc256b3a269cee5c407567556c463d0
Gitea <gitea@fake.local>
2020-12-19 22:40:02 +0700

    3

---
+++ Day 19: Monster Messages/puzzle.py
@@ -0,0 +1,10 @@
+import sys
+
+puzzle = [line for line in open(sys.argv[1]).read().split("\n\n")]
+rules = [s.split(":") for s in puzzle[0].split("\n")]
+rule = dict(rules)
+n = 0
+while True:
+  x = str(n)
+  rule[x].split())
+  break
