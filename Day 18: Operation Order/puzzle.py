1b3eedb0224ad25ccb06888785863a01b18714c1
Gitea <gitea@fake.local>
2020-12-19 16:23:01 +0700

    Day 18: Operation Order [Part Two]

--- Day 18: Operation Order/puzzle.py
+++ Day 18: Operation Order/puzzle.py
@@ -1,99 +1,46 @@
 import sys
 
-def operator_num(n):
-  if n == "+" or n == "-":
-    return 2
-  elif n == "(":
-    return 3
-  elif n == ")":
-    return 4
-  else:
-    return 2
-
-def add(num1,num2):
-  return num1 + num2
-
-def minus(num1,num2):
-  return num2 - num1
-
-def multi(num1,num2):
-  return num1 * num2
-
-def divide(num1,num2):
-  return num2 / num1
-
-def post_fix(*args):
-  for arg in args:
-    if arg not in operator:
-      arg = int(arg)
-      postfix.append(arg)
-    else:
-      if len(stack) == 0:
-        stack.append(arg)
-      else:
-        operator_value = operator_num(arg)
-        stack_value = operator_num(stack[-1])
-        if operator_value > stack_value:
-          if operator_value == 4:
-            while True:
-              postfix.append(stack.pop())
-              if stack[-1] == "(":
-                stack.pop()
-                break
-          else:
-            stack.append(arg)
-        elif operator_value < stack_value:
-          if stack[-1] == '(':
-            stack.append(arg)
-          else:
-            while len(stack) > 0:
-              if stack[-1] == 3:
-                break
-              postfix.append(stack.pop())
-            stack.append(arg)
-        elif operator_value == stack_value:
-          if stack_value == 3:
-            stack.append(arg)
-          else:
-            postfix.append((stack.pop()))
-            stack.append(arg)
-  while len(stack) > 0:
-    postfix.append(stack.pop())
-  return postfix
-
-def calculator_function(*args):
-  while len(postfix) > 1:
-    for operate in args:
-      if operate in operator:
-        arg_index = postfix.index(operate)
-        if operate == "+":
-          postfix.pop(arg_index)
-          result = add(postfix.pop(arg_index - 1), postfix.pop(arg_index - 2))
-          postfix.insert(arg_index - 2, result)
-        elif operate == "-":
-          postfix.pop(arg_index)
-          result = minus(postfix.pop(arg_index - 1), postfix.pop(arg_index - 2))
-          postfix.insert(arg_index - 2, result)
-        elif operate == "*":
-          postfix.pop(arg_index)
-          result = multi(postfix.pop(arg_index - 1), postfix.pop(arg_index - 2))
-          postfix.insert(arg_index - 2, result)
-        elif operate == "/":
-          postfix.pop(arg_index)
-          result = multi(postfix.pop(arg_index - 1), postfix.pop(arg_index - 2))
-          postfix.insert(arg_index - 2, result)
-      else:
-        continue
-  return postfix
-
-puzzle = [line.strip() for line in open(sys.argv[1], 'r')]
-operator = ["*", "/", "+", "-", "(", ")"]
-z = 0
-for x in puzzle:
-  postfix = []
+def post_fix(args):
   stack = []
-  y = x.replace("(","( ").replace(")"," )").split()
-  post_fix(*y)
-  n = int(calculator_function(*postfix)[0])
-  z += n
-print(z)
+  postfix = ''
+  for ch in args.replace(" ",""):
+    if ch not in operator:
+      postfix += ch
+    elif ch == '(':
+      stack.append('(')
+    elif ch == ')':
+      while stack and stack[-1] != '(':
+        postfix += stack.pop()
+      stack.pop()
+    else:
+      while stack and stack[-1] != '(' and operator_num[ch] <= operator_num[stack[-1]]:
+        postfix += stack.pop()
+      stack.append(ch)
+  while stack:
+    postfix += stack.pop()
+  return postfix
+
+def calculator_function(args):
+  stack = []
+  for ch in args:
+    if ch not in operator:
+      stack.append(int(ch))
+    else:
+      b = stack.pop()
+      a = stack.pop()
+      c = {'+':a+b, '-':a-b, '*':a*b, '/':a/b}[ch]
+      stack.append(c)
+  return stack[-1]
+
+operator = set(['+', '-', '*', '/', '(', ')'])
+puzzle = [line.strip() for line in open(sys.argv[1], 'r')]
+operator_num = {'+':2, '-':2, '*':2, '/':2}
+y = 0
+for exp in puzzle:
+  y += calculator_function(post_fix(exp.strip()))
+print("sum: {}".format(y))
+operator_num = {'+':2, '-':2, '*':1, '/':1}
+y = 0
+for exp in puzzle:
+  y += calculator_function(post_fix(exp.strip()))
+print("sum: {}".format(y))
